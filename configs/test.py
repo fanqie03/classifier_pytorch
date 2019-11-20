faab38d37c99c42a8c402ef1d08d5fbc0f41f0e0
mengfu188 <1242733702@qq.com>
2019-11-20 09:23:46 +0800

    - update config

--- configs/test.py
+++ configs/test.py
@@ -1,20 +1,65 @@
-
 train_datasets = [
     dict(type='ImageFolder',
-         root='/home/cmf/datasets/lfw/lfw-a')
+         root='/home/cmf/w_public/mmdetection/data/extract_video_tayg_0.97/train'),
+    # dict(type='ImageFolder',
+    #      root='/home/cmf/w_public/mmdetection/data/extract_video_tayg_0.97/train'),
+    # dict(type='ImageFolder',
+    #      root='/home/cmf/w_public/mmdetection/data/extract_video_tayg_0.97/train'),
+    dict(type='ImageFolder',
+         root='/home/cmf/datasets/helmet/helmet-wash/train')
 ]
 
 val_datasets = [
     dict(type='ImageFolder',
-         root='/home/cmf/datasets/lfw/lfw-a')
+         root='/home/cmf/datasets/helmet/helmet-wash/val')
 ]
 
-# import torchvision
-#
-# transform = [
-#     # torchvision.transforms.FiveCrop(50)
-#     torchvision.transforms.ColorJitter()
-#     dict(type='FiveCrop', size=50),
-#     dict(type='ToTensor')
-#
-# ]
+
+model = dict(
+    type='mobilenet_v2',
+    num_classes=2
+)
+
+train = dict(
+    pretrained_model=None,
+    freeze_body=False,
+    freeze_head=False,
+    resume_from=None,
+    init_type='xavier',
+    work_dir='checkpoint',
+    num_epochs=200,
+    batch_size=64,
+    start_epoch=0,
+    use_tensorboard=False,
+    val_epochs=1,
+    open_tensorboard=True
+)
+
+optimizer = dict(
+    type='torch.optim.SGD',
+    lr=0.0001,
+    momentum=0.9
+)
+
+transform = [
+    # torchvision.transforms.FiveCrop(50)
+    # torchvision.transforms.ColorJitter()
+    # dict(type='RandomAffine', degrees=30, scale=(0.5, 1.5), shear=5),
+    # dict(type='RandomHorizontalFlip'),
+    # dict(type='ColorJitter', brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
+    # dict(type='RandomGrayscale'),
+    # dict(type='iaa.CoarseDropout', p=(0.03, 0.15), size_percent=(0.1, 0.4)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='ToTensor'),
+    # dict(type='RandomErasing', ratio=(10, 15), inplace=True),
+    # dict(type='RandomErasing', ratio=(10, 15), inplace=True),
+    # dict(type='RandomErasing', ratio=(0.1, 0.2), inplace=True),
+    # dict(type='RandomErasing', ratio=(0.1, 0.2), inplace=True),
+
+]
+
+dataloader = dict(
+    batch_size=64,
+    num_workers=6,
+    shuffle=True
+)
